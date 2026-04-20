561d92f1e5700ceaac8755eaa61da942c8b745ed
Aboseif 2024 <mohamedaboseif272006@gmail.com>
2026-04-20 02:02:26 +0200

    add home page UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,53 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
+import "./css/home.css";
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
-    <main className="flex p-12 flex-col justify-center gap-4 items-center">
-      <h1 className="text-4xl">Probabilist</h1>
-      <p className="text-2xl pb-6 max-w-[40ch] text-center text-muted-foreground">
-        Probabilist is a tool designed to make your probability computing
-        easier, and effective.
+    <main className="container">
+
+      <h1 className="title">Probabilist</h1>
+
+      <p className="subtitle">
+        A modern statistical analysis web application inspired by SPSS.
       </p>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/preview">Preview</Link>
+
+      <p className="description">
+        The system helps you analyze data using probability models such as
+        binomial distribution, variance, expected value, and more.
+      </p>
+
+      <div className="items">
+
+        <div className="item">
+          <h3>🏗 Engineering</h3>
+          <p>
+            Analyze system reliability, component failure, and uptime probability.
+          </p>
+        </div>
+
+        <div className="item">
+          <h3>🏥 Medical</h3>
+          <p>
+            Evaluate clinical experiments and success rates in treatments.
+          </p>
+        </div>
+
+      </div>
+
+      <div className="buttons">
+
+        <Link to="/dashboard" className="btn primary">
+          Go to Dashboard
+        </Link>
+
+        <Link to="/preview" className="btn">
+          Preview
+        </Link>
+
+      </div>
+
     </main>
   )
 }
